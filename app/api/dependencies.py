ac0f84362ca4279ae0ec9338f00dd8cf38c86b65
naazzzim <nazimjabir@gmail.com>
2026-03-04 17:41:12 +0530

    removed unnecessary services

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,13 +1,7 @@
 from fastapi import Depends
 from app.services.map_service import MapService
-from app.services.user_service import UserService
 
 
 def get_map_service() -> MapService:
     """Dependency for MapService"""
     return MapService()
-
-
-def get_user_service() -> UserService:
-    """Dependency for UserService"""
-    return UserService()
